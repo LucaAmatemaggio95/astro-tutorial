f0ca9ca568a6f560682a219ebe42645070c5904d
lamatemaggio <luca.amatemaggio@rcslab.it>
2023-10-24 12:56:29 +0200

    Addes SSR

---
+++ src/pages/api/test.ts
@@ -0,0 +1,8 @@
+export async function GET() {
+  return new Response(JSON.stringify({ msg: "Hello World" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
